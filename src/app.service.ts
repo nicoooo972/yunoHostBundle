4f4529165d0a2fc462e264a1b8103f3a18187f27
lebozec <nicolas.lebozec@xelians.fr>
2024-01-10 16:36:43 +0100

    install avec fichier

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as toml from '@iarna/toml';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getData(): any[] {
+    const data = fs.readFileSync('./test.toml', 'utf-8');
+
+    try {
+      const tomlParsed = toml.parse(data);
+      if (tomlParsed && tomlParsed.bundle) {
+        const bundleData = tomlParsed.bundle;
+        return [bundleData];
+      } else {
+        console.error('La clé "bundle" n\'existe pas dans le fichier TOML.');
+        return [];
+      }
+    } catch (error) {
+      console.error(
+        'Erreur lors de la lecture du fichier TOML :',
+        error.message,
+      );
+      return [];
+    }
+    // const dataParse = JSON.parse(data);
+    // if (dataParse && dataParse.bundle) {
+    //   // Accédez à la clé "bundle" pour obtenir les données spécifiques
+    //   const bundleData = dataParse.bundle;
+
+    //   // Vous pouvez également retourner l'objet "bundleData" complet si nécessaire
+    //   return [bundleData];
+    // } else {
+    //   console.error('La clé "bundle" n\'existe pas dans le JSON.');
+    //   return [];
+    // }
   }
 }
--- src/install/install.service.ts
+++ src/install/install.service.ts
@@ -1,11 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { spawn } from 'child_process';
-import { PrismaClient } from '@prisma/client';
+//import { PrismaClient } from '@prisma/client';
+import { AppService } from 'src/app.service';
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 @Injectable()
 export class InstallService {
+  constructor(private readonly appService: AppService) {}
+
   async installApplication(): Promise<void> {
+    const applications = this.appService.getData();
+    console.log(applications);
+    try {
+      for (const application of applications) {
+        const { appName, domain, password, admin } = application;
+
+        const command = `ssh nicoco@dcm1tlg1.nohost.me "echo '12345azerty' | sudo -S yunohost app install ${appName} --args '${domain}&path=/${appName}&init_main_permission=admins&password=${password}&admin=${admin}'"`;
+
+        const childProcess = spawn('bash', ['-c', command]);
+
+        childProcess.stdout.on('data', (data) => {
+          console.log(`Données de sortie : ${data}`);
+        });
+
+        childProcess.stderr.on('data', (data) => {
+          console.error(`Erreur de sortie : ${data}`);
+        });
+
+        childProcess.on('close', (code) => {
+          if (code === 0) {
+            console.log(`Installation réussie de ${appName}`);
+          } else {
+            console.error(
+              `Erreur lors de l'installation de ${appName}, code de sortie : ${code}`,
+            );
+          }
+        });
+      }
+    } catch (error) {
+      console.error(
+        "Erreur lors de l'installation des applications:",
+        error.message,
+      );
+    }
+  }
+
+  /* async installApplication(): Promise<void> {
     const applications = await prisma.application.findMany();
     try {
       for (const application of applications) {
@@ -40,5 +80,5 @@ export class InstallService {
         error.message,
       );
     }
-  }
+  }*/
 }
