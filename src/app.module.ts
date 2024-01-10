1ce653b29ba828877cdb024aa4fca2e6b6a642cd
AlanGarnier <a.garnier164@gmail.com>
2024-01-10 16:54:06 +0100

    Add script to get apps

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppsController} from "./apps/apps.controller";
+import { AppsService } from './apps/apps.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AppsController],
+  providers: [AppService, AppsService],
 })
 export class AppModule {}
