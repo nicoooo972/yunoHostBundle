5695e349b9c398368a7a7a286cedeebc17fa71b9
lebozec <nicolas.lebozec@xelians.fr>
2024-01-11 18:47:07 +0100

    tgwcxkjcw

--- frontend/src/components/Install.tsx
+++ frontend/src/components/Install.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
@@ -6,71 +6,94 @@ import DialogActions from '@mui/material/DialogActions';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 interface InstallForm {
-    onClose: () => void;
-    open: boolean;
+  onClose: () => void;
+  open: boolean;
 }
 
 const Install: React.FC<InstallForm> = ({ onClose, open }) => {
-    const [nomAdmin, setNomAdmin] = useState('');
-    const [nomDomaine, setNomDomaine] = useState('');
-    const [motDePasse, setMotDePasse] = useState('');
+  const [admins, setAdmins] = useState<string[]>([]);
+  const [domains, setDomains] = useState<string[]>([]);
+  const [selectedAdmin, setSelectedAdmin] = useState<string>('');
+  const [selectedDomain, setSelectedDomain] = useState<string>('');
 
-    const handleSubmit = () => {
-        console.log('Formulaire soumis avec les valeurs :', { nomAdmin, nomDomaine, motDePasse });
-        onClose();
+  useEffect(() => {
+    // Récupération des noms et des domaines de la route get
+    const getAdminsData = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:3000/install/users-admin',
+        );
+        console.log(response);
+        const data = await response.json();
+
+        setAdmins(Object.keys(data.admins.users));
+
+        const response2 = await fetch('http://localhost:3000/install/domain');
+        console.log(response2);
+        const data2 = await response2.json();
+
+        setDomains(data2.domain.domains);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
+    getAdminsData(); // Call  the fetchData function directly
+  }, []);
 
-    return (
-        <Dialog open={open} onClose={onClose}>
-            <DialogTitle>Remplissez le formulaire d'installation</DialogTitle>
-            <DialogContent>
-                {/* Using InputLabel to provide a label for the Select */}
-                <InputLabel htmlFor="nom-administrateur">Nom de l'administrateur</InputLabel>
-                <Select
-                    id="nom-administrateur"
-                    variant="outlined"
-                    fullWidth
-                    value={nomAdmin}
-                    onChange={(e) => setNomAdmin(e.target.value as string)}
-                >
-                    {/* Example list of options, replace with your actual data */}
-                    <MenuItem value="admin1">Admin 1</MenuItem>
-                    <MenuItem value="admin2">Admin 2</MenuItem>
-                    <MenuItem value="admin3">Admin 3</MenuItem>
-                </Select>
+  const handleSubmit = () => {
+    console.log('Formulaire soumis avec les valeurs :', { selectedAdmin });
+    onClose();
+  };
 
-                <TextField
-                    label="Nom de domaine"
-                    variant="outlined"
-                    fullWidth
-                    value={nomDomaine}
-                    onChange={(e) => setNomDomaine(e.target.value)}
-                    margin="normal"
-                />
-                <TextField
-                    label="Mot de passe"
-                    variant="outlined"
-                    fullWidth
-                    type="password"
-                    value={motDePasse}
-                    onChange={(e) => setMotDePasse(e.target.value)}
-                    margin="normal"
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={onClose} color="primary">
-                    Annuler
-                </Button>
-                <Button onClick={handleSubmit} color="primary">
-                    Valider
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Remplissez le formulaire d'installation</DialogTitle>
+      <DialogContent>
+        {/* Using InputLabel to provide a label for the Select */}
+        <InputLabel htmlFor="nom-administrateur">
+          Nom de l'administrateur
+        </InputLabel>
+        <Select
+          id="nom-administrateur"
+          variant="outlined"
+          fullWidth
+          value={selectedAdmin}
+          onChange={(e) => setSelectedAdmin(e.target.value as string)}
+        >
+          {admins.map((user, index) => (
+            <MenuItem key={index} value={user}>
+              {user}
+            </MenuItem>
+          ))}
+        </Select>
+        <InputLabel htmlFor="nom-administrateur">Nom du domaine</InputLabel>
+        <Select
+          id="domain"
+          variant="outlined"
+          fullWidth
+          value={selectedDomain}
+          onChange={(e) => setSelectedDomain(e.target.value as string)}
+        >
+          {domains.map((main, index) => (
+            <option key={index} value={main}>
+              {main}
+            </option>
+          ))}
+        </Select>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose} color="primary">
+          Annuler
+        </Button>
+        <Button onClick={handleSubmit} color="primary">
+          Valider
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
 
 export default Install;
