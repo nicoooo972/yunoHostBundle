4fbd3389d35c896f05e9c049bff4898f55827dc9
lebozec <nicolas.lebozec@xelians.fr>
2024-01-12 13:54:44 +0100

    front correction

--- frontend/src/pages/Historique.tsx
+++ frontend/src/pages/Historique.tsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from 'react';
 
-interface Application {
-  index: number;
-  application: string[];
+interface BundlesInstallerProps {
+  // Add any specific props if needed
 }
 
-const BundlesInstaller: React.FC = () => {
-  const [data, setData] = useState<Application[]>([]);
+const BundlesInstaller: React.FC<BundlesInstallerProps> = () => {
+  const [data, setData] = useState<string[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/install/installed-apps')
@@ -14,7 +13,7 @@ const BundlesInstaller: React.FC = () => {
       .then((data) => {
         // Mise à jour de l'état avec les données récupérées
         setData(data.installedApps);
-        console.log(data.installedApps);    
+        console.log(data.installedApps);
       })
       .catch((error) => {
         console.error('Erreur lors de la récupération des données :', error);
@@ -22,37 +21,23 @@ const BundlesInstaller: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      <div
-        style={{
-          width: '1080px',
-          margin: 'auto',
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'flex-start',
-          flexWrap: 'wrap',
-          justifyContent: 'center',
-          gap: '4rem',
-        }}
-      >
-        {data.map((application, index) => (
-          <div key={index} style={{ marginBottom: '20px' }}>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                marginBottom: '10px',
-              }}
-            >
-              <div>
-                <h3>{application}</h3>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+    <>
+      <h1 className='text-2xl md:text-4xl pt-20 font-semibold'>Vos applications Installées</h1>
+<div className='grid grid-cols-2 gap-10 pt-10'>
+  {data.map((application, index) => (
+    <div className='flex px-4 py-2 rounded-md shadow-lg bg-gray-50 items-center justify-between w-1/2 mx-auto' key={index}>
+      <li className='flex mr-12 py-2 justify-center text-lg font-semibold'>{application}</li>
+      <svg width="30" height="30" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+        <g fill="none" stroke="#32cd44" stroke-linecap="round" stroke-linejoin="round">
+          <path d="m4 8l2.05 1.64a.48.48 0 0 0 .4.1a.5.5 0 0 0 .34-.24L10 4" />
+          <circle cx="7" cy="7" r="6.5" />
+        </g>
+      </svg>
     </div>
+  ))}
+</div>
+
+    </>
   );
 };
 
